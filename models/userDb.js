8e45e7ab6041b1df14df4e6c2acbc8024dc9a27c
Caleb Redd <caleb.redd@yahoo.com>
2019-08-25 20:59:32 -0700

    Fixed api db issues

--- models/userDb.js
+++ models/userDb.js
@@ -1,4 +1,4 @@
-const db = require('../data/dbConfig.js');
+const db = require("../data/dbConfig.js");
 
 module.exports = {
   get,
@@ -11,64 +11,59 @@ module.exports = {
   findBy,
   getPostsById,
   getPortfolioById,
-  removePost,
+  removePost
 };
 
 function get() {
-  return db('stylists')
+  return db("stylists");
 }
 
-function getAllUsers(){
-  return db('users')
-  .select('email', 'stylist')
+function getAllUsers() {
+  return db("users").select("email", "stylist");
 }
 
 function findBy(filter) {
-  return db('users')
-  .where(filter);
+  return db("users").where(filter);
 }
 
 function getById(id) {
-  return db('stylists')
-  .where('stylists.id', id)
-  .first()
+  return db("stylists")
+    .where("stylists.id", id)
+    .first();
 }
 
 function getPostsById(id) {
-  return db('posts')
-  .where('posts.stylists_id', id)
+  return db("posts").where("posts.stylists_id", id);
 }
 
 function getPortfolioById(id) {
-  return db('portfolio')
-  .where('portfolio.stylists_id', id)
+  return db("portfolio").where("portfolio.stylists_id", id);
 }
 
 function insert(user) {
-  return db('posts')
-    .insert(user)
+  return db("users").insert(user);
 }
 
 function update(id, changes) {
-  return db('users')
-    .where('id', id)
+  return db("users")
+    .where("id", id)
     .update(changes);
 }
 
 function updatePost(id, changes) {
-  return db('posts')
-    .where('id', id)
+  return db("posts")
+    .where("id", id)
     .update(changes);
 }
 
 function remove(id) {
-  return db('users')
-    .where('id', id)
+  return db("users")
+    .where("id", id)
     .del();
 }
 
-function removePost(id){
-  return db('posts')
-  .where('id', id)
-  .del();
+function removePost(id) {
+  return db("posts")
+    .where("id", id)
+    .del();
 }
